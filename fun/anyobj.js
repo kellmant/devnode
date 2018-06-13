8969656c5e1bac3e9b0fdb2fb9a28a1492797266
kellman <kellman@meghu.ca>
2018-06-13 01:22:42 -0400

    clean up and append new parse option

--- fun/anyobj.js
+++ fun/anyobj.js
@@ -8,13 +8,6 @@ module.exports = async (x) => {
 			for (var j in x[i]) {
 				const myObjs = Object.keys(x[i][j]).reduce((p, c) => ({...p, [c]: x[i][j][c]}), {})
 				var cpType = myObjs.type
-				//if (myObjs.state) {
-				//	var cpUid = myObjs.uid
-				//	var cpName = myObjs.name
-				//} else {
-				//	var cpName = myObjs.name
-				//	var cpUid = myObjs.uid
-				//}
 				let cpData = new Objectclass(myObjs)
 				if (cpType == 'host') {
 					cpRes = cpData.host(myObjs)
@@ -25,10 +18,6 @@ module.exports = async (x) => {
 				} else {
 					continue
 				}
-				//myObjs.mycmd = '/' + cpName
-				//await console.dir(cpRes)
-				//await console.log('\n')
-				//return myObjs
 			}
 		}
 		return cpRes
@@ -37,4 +26,6 @@ module.exports = async (x) => {
 	}
 }
 
+
+//Object.entries(obj).forEach(([key, value]) => console.log(`${key}: ${value}`)); 
 
