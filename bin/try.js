ad71ba4c6480662d8afc9784ad3a20cc9fca9af6
kellmant <kellman@meghu.ca>
2018-06-24 05:43:03 -0400

    foreach scan of object

--- bin/try.js
+++ bin/try.js
@@ -33,7 +33,7 @@ module.exports = async (args) => {
 		let myValue = await myKeystore.getKey(mykey)
 		//let myValue = JSON.parse(await myKeystore.resVal())
 		Object.entries(myValue.node.nodes).forEach(([key, value]) => {
-			console.log(value)
+			console.log(`this key ${key} : ${value}`)
 		})
 	} catch (err) {
 		console.log('ERROR IN SESSION event : ' + err.message)
