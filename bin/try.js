b92bfc5e23009a403e21457cdae81354e4982dbb
kellmant <kellman@meghu.ca>
2018-06-24 05:17:44 -0400

    new ctrl test

--- bin/try.js
+++ bin/try.js
@@ -11,122 +11,36 @@
 // this will show you return values of your args 
 // for runtime
 //
-const delay = async () => {
-	const incmd = {'_':['login', 'opb']}
-	const startup = await require('../bin/login')(incmd)
-	return await startup
-}
-const myoffset = 0
-const pglimit = 500
-const details = 'uid'
 const path = require('path');
 const scriptname = path.basename(__filename);
 const classcall = `../class/${scriptname}`
 //const myClass = require(classcall)
-
 const doParse = require('../fun/testobj')
-
-const cpLive = require('../fun/session')
-const Cpapi = require('../class/cpapi')
 const Keystore = require('../class/keystore')
 const Cpobject = require('../class/object')
-let mycmd = 'show-objects'
+let mykey = 'tags'
 
 // example runtime for your class method
 //
 
 module.exports = async (args) => {
 	try {
-		let newcpdata = {}
-		await delay()
 		if (args._[1]) {
 			console.log(args._[1])
-			mycmd = args._[1]
+			mykey = args._[1]
 		}
-		if (args.unused) {
-			console.log('showing unused objects')
-			mycmd = 'show-unused-objects'
-		}
-		const cpSession = await cpLive()
-		await console.dir(cpSession)
-		if (!cpSession.uid) {
-			require('../bin/login')
-			console.log('No Active Session, please login')
-		}
-		let Myapi = await new Cpapi(cpSession)
-		await Myapi.print()
-		await Myapi.setCnt(myoffset, pglimit)
-		await Myapi.setDetail(details)
-		if (args) {
-		await Myapi.addData(args)
-		}
-		await Myapi.setCmd(mycmd)
-		await Myapi.print()
-		let mycpres = await Myapi.apiPost()
-		let parsedObj = []
-		parsedObj.push(await doParse(mycpres.objects))
-		if (mycpres.total > mycpres.to) {
-			let inoffset = Number(myoffset) + Number(pglimit)
-			while (mycpres.total > inoffset) {
-				await Myapi.setCnt(inoffset, pglimit)
-				mycpres = await Myapi.apiPost()
-				parsedObj.push(await doParse(mycpres.objects))
-				inoffset = Number(inoffset) + Number(pglimit)
-			}
-		}
-		if (mycmd === 'show-unused-objects') {
-			args.filter = 'unused'
-			args.type = 'object'
-			if (!args.tags) {
-				args.tags = 'unused'
-			}
-		}
-		if (!args.filter) {
-			args.filter = 'all'
-		}
-		if (!args.type) {
-			args.type = 'object'
-		}
-
 		let mycnt = 0
-		var newArray = Array.from(Object.values(parsedObj))
-		for (var i in newArray) {
-			for (var j in newArray[i]) {
-				let myUid = 'uid/' + newArray[i][j]
-				const myObj = new Keystore()
-				await myObj.getKey(myUid)
-				let myCached = JSON.parse(await myObj.resVal())
-				
-				//console.log(' obj/' + args.filter + '/' + newArray[i][j] + ' = ' + args.type)
-				//await console.log(myCached.type)
-				const myNewobj = new Cpobject(myCached)
-				await myNewobj.host(myCached)
-				await myNewobj.network(myCached)
-				//await myNewobj.tag(args.filter)
-				if (args.tags) {
-				await myNewobj.tag(args.tags)
-				}
-				let objDump = await myNewobj.dump()
-				let cpTagged = {
-					'mkey' : 'tag/' + args.filter + '/set-' + myCached.type + '/' + myCached.uid,
-					'mvalue' : JSON.stringify(objDump),
-					'tagkey' : 'obj/tag/' + args.filter,
-					'tagvalue' : JSON.stringify(args.tags)
-				}
-
-				await myObj.setKey(cpTagged.mkey, cpTagged.mvalue)
-				mycnt++
-				await myObj.setKey(cpTagged.tagkey, cpTagged.tagvalue)
-			}
-		}
-		console.log(mycnt)
+		const myKeystore = new Keystore()
+		await myKeystore.getKey(mykey)
+		let myValue = JSON.parse(await myObj.resVal())
+		console.dir(myValue)
 	} catch (err) {
 		console.log('ERROR IN SESSION event : ' + err.message)
 		console.log(err)
 	} finally {
-		let runcmd = {'_':['logout']}
-		require('../bin/logout')(runcmd)
-		return
+		//let runcmd = {'_':['logout']}
+		//require('../bin/logout')(runcmd)
+		console.log('try runtime finally done.')
 	}
 }
 
