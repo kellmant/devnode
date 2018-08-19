f02ab36f780b034b3ba9aef810fbf8d45aabde7b
kellman <kellman@meghu.ca>
2018-08-19 00:32:47 -0400

    test new object call options

--- bin/tt.js
+++ bin/tt.js
@@ -6,18 +6,41 @@ const scriptname = path.basename(__filename);
 //const Rejson = require('../fun/rejson')
 const doWrite = require('../fun/writefile')
 
-const mytask = require('../playground/task.json')
-const taskid = mytask[0]['task-id']
+//const diff = require('hyperdiff')
+//const diff = require('comparison-object-diff')
+//const diff = require('just-diff')
+//const diff = require('deep-diff').diff;
+//
+const groupBy = require('json-groupby')
+
+const diff = require('diff-arrays-of-objects');
+const removeDuplicates = require('removeduplicates');
+const ddiff = require("deep-object-diff").diff;
+const addedDiff = require("deep-object-diff").addedDiff;
+const deletedDiff = require("deep-object-diff").deletedDiff;
+const detailedDiff = require("deep-object-diff").detailedDiff;
+const updatedDiff = require("deep-object-diff").updatedDiff;
+const sortobject = require('deep-sort-object');
+const sort = require('sort-any');
+const objTry = require('../playground/try.json')
+const objDmp = require('../playground/dump.json')
+//const Obja = require('../playground/store/rulebase01.json')
+const objPolicy = require('../playground/policy.json')
 
 
 module.exports = async () => {
 	try {
-		var myarr = []
-		var myvalue = {}
-		console.dir(await mytask)
-		console.log(await taskid)
-		//await doWrite(args._[1], myarr)
-		return taskid
+		const mysorted = sortobject(objTry)
+		await doWrite('sorted', mysorted)
+		const mygrp = groupBy(objDmp.rulebase, ['rulebase.type'])
+		await doWrite('group', mygrp)
+		//const mydedup = removeDuplicates(objTry)
+		//await doWrite('dedup', mydedup)
+		const mydiff = diff(objPolicy, objTry)
+		//console.dir(await mydiff)
+		console.log(await typeof mydiff)
+		await doWrite('policy-diff', mydiff)
+		return mysorted
 	} catch (err) {
 		console.log(err.message)
 		throw new Error(err)
